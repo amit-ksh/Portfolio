bb8d6972850ddf18e702b89b280963b99bd28072
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2022-11-04 12:36:52 +0530

    add new svg and techstack in project card

--- components/Project.vue
+++ components/Project.vue
@@ -4,34 +4,49 @@
         <img class="absolute inset-0 h-full w-full object-cover" :src="imageUrl" :alt="title">
     </div>
     <div class="p-4">
-        <h4 class="mt-2 mb-2 font-bold">{{ title }}</h4>
+        <h2 class="text-xl mt-2 mb-2 font-bold">{{ title }}</h2>
         <p class="text-sm font-mono">{{ description }}</p>
     </div>
-    <div class="flex flex-wrap flex-row mt-auto py-4 px-2 border-t border-b text-xs text-gray-700">
-        <!-- GITHUB LINK -->
-        <a
-        v-if="url.github"
-        :href="url.github"
-        class="
-          ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300
-          focus:text-slate-500 dark:focus:text-slate-300
-        "
-        target="_blank"
-        >
-          <GithubLogo :label="title + ' on GitHub'" class="w-5 h-5" />
-        </a>
-        <!-- WEBSITE LINK -->
-        <a
-        v-if="url.website"
-        :href="url.website"
-        class="
-          ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300
-          focus:text-slate-500 dark:focus:text-slate-300
-        "
-        target="_blank"
-        >
-          <WebLogo :label="title + '\'s Website'" class="w-5 h-5" />
-        </a>
+    <div class="mt-auto">
+      <div class="px-4 py-2">
+        <h3 class="font-semibold">Tech Stack</h3>
+
+        <div class="flex gap-4 flex-wrap my-2">
+          <img 
+            v-for="tech in techStack" 
+            key="tech" 
+            :src="`/assets/svg/${tech}.svg`" 
+            width="30" 
+            height="30" 
+          />
+        </div>
+      </div>
+      <div class="flex flex-wrap flex-row py-4 px-2 border-t border-b text-xs text-gray-700">
+          <!-- GITHUB LINK -->
+          <a
+          v-if="url.github"
+          :href="url.github"
+          class="
+            ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300
+            focus:text-slate-500 dark:focus:text-slate-300
+          "
+          target="_blank"
+          >
+            <GithubLogo :label="title + ' on GitHub'" class="w-5 h-5" />
+          </a>
+          <!-- WEBSITE LINK -->
+          <a
+          v-if="url.website"
+          :href="url.website"
+          class="
+            ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300
+            focus:text-slate-500 dark:focus:text-slate-300
+          "
+          target="_blank"
+          >
+            <WebLogo :label="title + '\'s Website'" class="w-5 h-5" />
+          </a>
+      </div>
     </div>
   </div>
 </template>
@@ -44,6 +59,7 @@ defineProps({
   title: String,
   description: String,
   imageUrl: String,
+  techStack: Array,
   url: {
     github: String,
     website: String,
--- pages/work.vue
+++ pages/work.vue
@@ -17,7 +17,8 @@
           <Project 
             :title="project.title" 
             :description="project.description" 
-            :imageUrl="project.image.url" 
+            :techStack="project.techStack"
+            :imageUrl="project.image" 
             :url="project.url" 
           />
         </div>
@@ -34,55 +35,50 @@ import personImg from '~/assets/images/person.jpg'
 
 const projects = [
   {
-    image: {
-      url: iphoneImg
-    },
+    image: iphoneImg,
     title: 'Locknest',
     description: 'Locknest is the Progressive Web App(PWA) application that allows users to store and manage their passwords and other personal information like bank accounts and payment card infos in a single place. In short, it is the online password manager or digital wallet for the users.',
+    techStack: ['nextjs', 'typescript', 'chakraui', 'prisma', 'postgres', 'vercel'],
     url: {
       github: 'https://github.com/amit-ksh/locknest',
       website: 'https://locknest.vercel.app/',
     }
   },
   {
-    image: {
-      url: laptopImg
-    },
+    image: laptopImg,
     title: 'Weather App',
     description: 'Weather App is a React web app which display the weather details from all around the world.',
+    techStack: ['react', 'node', 'chakraui', 'netlify'],
     url: {
       github: 'https://github.com/amit-ksh/weather-app',
       website: 'https://your-weatherman.netlify.app/',
     }
   },
   {
-    image: {
-      url: personImg
-    },
+    image: personImg,
     title: 'Todo App',
     description: 'A todo app is a simple web application where user can create todo list and add task/todo to the list. User can mark an added task as completed and also able to delete the todo list and task.',
+    techStack: ['html', 'css', 'js', 'node', 'postgres', 'heroku'],
     url: {
       github: 'https://github.com/amit-ksh/todo-app',
       website: 'https://h-todo-app.herokuapp.com/',
     }
   },
   {
-    image: {
-      url: iphoneImg
-    },
+    image: iphoneImg,
     title: 'GnB Distribution Analyzer',
     description: 'A python package that helps to analyze the Gaussian and Binomial Distributions',
+    techStack: ['python', 'numpy', 'matplotlib'],
     url: {
       github: 'https://github.com/amit-ksh/Udacity-AI-Programming-with-Python/tree/main/mini-projects/GnB_Distribution',
       website: 'https://pypi.org/project/gnb-distributions-analyzer/'
     }
   },
   {
-    image: {
-      url: laptopImg
-    },
+    image: laptopImg,
     title: 'Portfolio',
     description: 'Personal site showcasing my projects and online resume.',
+    techStack: ['nuxt', 'tailwindcss', 'threejs', 'gsap', 'netlify'],
     url: {
       github: 'https://github.com/amit-ksh/Portfolio/',
       website: 'https://amit-ksh.netlify.app/',
@@ -90,7 +86,6 @@ const projects = [
   },
 ];
 
-const titleRef = ref(null)
 const projectsRef = ref(null)
 
 onMounted(() => {
